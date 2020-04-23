af0ba089ee1278cc667db824cf23d6faa00c523b
Joan <johan.rulle@laposte.net>
2020-04-23 18:35:59 +0200

    refactor: architecture backend

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/gif" href="./images/favico.png" />
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Raccourcisseur d'Url</title>
+</head>
+<body>
+    <header>
+        <img id="header-logo" src="./images/logo.png" alt="logo">
+
+        <div id="header-first-div">
+            <h1>Une url longue ? raccourcissez-là</h1>
+
+            <p>Largement meilleur et plus court que les autres.</p>
+        </div>
+
+        <form action="#" method="post">
+            <input id="header-url" type="url" name="url" placeholder="Coller un lien à raccourcir...">
+            <input id="header-submit" type="submit" value="Raccourcir">
+        </form>
+
+        <div id="header-last-div">
+            <p>url raccourcie :</p>
+        </div>
+    </header>
+
+    <main>
+        <div>
+            <h2>Ces marques nous font confiance</h2>
+        </div>
+
+        <div id="main-images">
+            <ul>
+                <li><img src="./images/1.png" alt="logo"></li>
+                <li><img src="./images/2.png" alt="logo"></li>
+                <li><img src="./images/3.png" alt="logo"></li>
+                <li><img src="./images/4.png" alt="logo"></li>
+            </ul>
+        </div>
+    </main>
+
+    <footer>
+        <div>
+            <img src="./images/logo2.png" alt="logo_billy">
+            <p>2020 &#169; Billy</p>
+            <p>Contact - À Propos</p>
+        </div>
+    </footer>
+</body>
+</html>
