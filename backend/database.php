8c15b8f67931d03eb849b64bd90e8ca0c6b3732a
Joan <johan.rulle@laposte.net>
2020-04-24 19:50:38 +0200

    feat: creation database

---
+++ backend/database.php
@@ -0,0 +1,12 @@
+<?php
+
+try {
+
+    $bdd = new PDO('mysql:host=localhost; dbname=udemy; charset=utf8', 'root', '');
+
+} catch (PDOException $e) {
+
+    die('Erreur à la connexion à la base de donnée '. $e->getMessage());
+
+}
+
